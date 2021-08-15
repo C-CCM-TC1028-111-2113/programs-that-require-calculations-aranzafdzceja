164bc4b365bbdc118db6c140a4e25bfef6bdea26
aranzafdzceja <88851200+aranzafdzceja@users.noreply.github.com>
2021-08-15 18:01:18 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,7 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    entrada1 = int(input("Dame un primer número entero: "))
+    entrada2 = int(input("Dame un segundo número entero: "))
 
+    suma = entrada1 + entrada2
+    resta = entrada1 - entrada2
+    multiplicacion = entrada1 * entrada2
+
+    print("Suma: " + str(suma))
+    print("Resta: " + str(resta))
+    print("Multiplicación: " + str(multiplicacion))
 
 if __name__ == '__main__':
     main()
