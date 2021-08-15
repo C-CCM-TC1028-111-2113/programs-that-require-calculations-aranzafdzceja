6a8bf0feab47ed3a5ef2e71481008071b66de486
aranzafdzceja <88851200+aranzafdzceja@users.noreply.github.com>
2021-08-15 18:13:56 -0500

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,8 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    b = int(input("Dame la base del triángulo: "))
+    h = int(input("Dame la altura del triángulo: "))
+
+    area = b * h / 2
+
+    print("Área: " + str(area))
 
 
-
-if __name__ == '__main__':
+if __name__=='__main__':
     main()
+
